a52c8ce5c7d656e6f4fca19be62b862f72c26819
Jim Gay <jim@saturnflyer.com>
2009-02-16 12:05:55 -0500

    properly scope tags

--- app/models/aggregated_archive_month.rb
+++ app/models/aggregated_archive_month.rb
@@ -13,19 +13,43 @@ class AggregatedArchiveMonth < Page
   
   include ArchiveIndexTagsAndMethods
   desc %{
-      Grants access to a subset of the children of the archive page
+      Grants access to a subset of the children of the aggregated pages
       that match the specific month which the index page is rendering.
       
       *Usage*:
-       <pre><code><r:archive:children>...</r:archive:children></code></pre>
+       <pre><code><r:aggregate:archive:children>...</r:aggregate:archive:children></code></pre>
   }
-  tag "archive:children" do |tag|
+  tag "aggregate:archive:children" do |tag|
     year, month = $1, $2 if request_uri =~ %r{/(\d{4})/(\d{2})/?$}
     start = Time.local(year, month)
     finish = start.next_month
     parent_ids = tag.locals.parent_ids || [parent_id] || [Page.find_by_url('/').id]
     tag.locals.children = Page.find(:all,:conditions => ["published_at >= ? and published_at < ? and parent_id IN (?)",start,finish,parent_ids])
     tag.expand
+  end
+  
+  desc %{
+    Renders the contained block for each child of the aggregated pages 
+    for the current index.  Accepts the same options as the plain 
+    @<r:children:each />@.
+
+    *Usage*:
+    
+    <pre><code><r:aggregate urls="/section1; /section2; /section3">
+      <r:archive:children:each>
+        ...
+      </r:archive:children:each>
+    </r:aggregate></code></pre>
+  }
+  tag "aggregate:archive:children:each" do |tag|
+    tag.locals.previous_headers = {}
+    output = ''
+    tag.locals.children.each do |child|
+      tag.locals.page = child
+      tag.locals.child = child
+      output << tag.expand
+    end
+    return output
   end
   
 end
